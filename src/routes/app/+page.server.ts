6e97250421816dce37629ec3400304865abefed0
Matej Bačo <matejbaco2000@gmail.com>
2022-12-11 16:46:47 +0100

    Improve cookies delete logic

--- src/routes/app/+page.server.ts
+++ src/routes/app/+page.server.ts
@@ -3,8 +3,14 @@ import { redirect } from "@sveltejs/kit";
 import type { Actions } from "./$types";
 
 export const actions: Actions = {
-	logout: async ({ cookies }) => {
-		cookies.delete(appwriteCookie);
+	logout: async ({ cookies, url }) => {
+		cookies.delete(appwriteCookie, {
+			secure: true,
+			httpOnly: true,
+			path: '/',
+			domain: (url.hostname !== 'localhost' ? '.' : '') + url.hostname,
+			// expires: expiry
+		});
 		throw redirect(307, '/auth');
 	}
 };
