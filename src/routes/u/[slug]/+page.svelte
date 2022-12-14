ada0030904ee6303076504260ef65d295dc64d65
Matej Bačo <matejbaco2000@gmail.com>
2022-12-14 14:54:13 +0100

    Improve cover, allow custom colors

--- src/routes/u/[slug]/+page.svelte
+++ src/routes/u/[slug]/+page.svelte
@@ -1,6 +1,22 @@
 <script lang="ts">
 	import type { PageData } from './$types';
 	export let data: PageData;
+
+	function getClasses(type: string) {
+		return type === 'green'
+			? 'bg-green-500 text-white'
+			: type === 'red'
+			? 'bg-red-500 text-white'
+			: type === 'pink'
+			? 'bg-pink-500 text-white'
+			: type === 'blue'
+			? 'bg-blue-500 text-white'
+			: type === 'black'
+			? 'bg-black text-white'
+			: type === 'white'
+			? 'bg-white text-black'
+			: '';
+	}
 </script>
 
 <div class="bg-white p-3 py-12 rounded-b-xl">
@@ -16,24 +32,17 @@
 	{#each data.links.documents as link}
 		<a href={link.url} rel="noreferrer">
 			<button
-				class={`${
-					link.type === 'green'
-						? 'bg-green-500 text-white'
-						: link.type === 'red'
-						? 'bg-red-500 text-white'
-						: link.type === 'pink'
-						? 'bg-pink-500 text-white'
-						: link.type === 'blue'
-						? 'bg-blue-500 text-white'
-						: link.type === 'black'
-						? 'bg-black text-white'
-						: 'bg-white text-black'
-				} w-full p-4 rounded-xl shadow-sm`}>{link.text}</button
+				style={`${getClasses(link.type) === '' ? link.type : ''}`}
+				class={`${getClasses(link.type)} w-full p-4 rounded-xl shadow-sm`}>{link.text}</button
 			>
 		</a>
 	{/each}
 
-	<div class={`${data.links.documents.length <= 0 ? '' : 'border-t-2'} max-w-sm mx-auto mt-6 border-gray-200 pt-4`}>
+	<div
+		class={`${
+			data.links.documents.length <= 0 ? '' : 'border-t-2'
+		} max-w-sm mx-auto mt-6 border-gray-200 pt-4`}
+	>
 		<p class="text-gray-400 text-sm text-center">
 			Create your own page <a class="text-black" href="/">Here</a>. It's 100% free.
 		</p>
